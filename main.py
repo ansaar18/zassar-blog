5138044968579b74744dc42cc09977e36b09d6cc
ansaarz <ansartoofanee@gmail.com>
2021-08-02 16:31:02 +0400

    Chnage Database key and value pair

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ gravatar = Gravatar(app,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1', "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -270,5 +270,5 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
